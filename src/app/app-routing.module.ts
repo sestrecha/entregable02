9b62cb68588d2005f6a9556f36680ccb1a53b1e7
maria15bm <100429055@alumnos.uc3m.es>
2022-04-16 23:01:03 +0200

    Título y menú inferior. Rooting perfil.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PerfilComponent } from './perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'perfil',
+    component:PerfilComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
