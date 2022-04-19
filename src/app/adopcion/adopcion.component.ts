f78b30abdca81111b456530a43cf2598a404b8de
marta <100429066@alumnos.uc3m.es>
2022-04-19 16:49:39 +0200

    intento de hacer la tl pero soy yo llorando

--- src/app/adopcion/adopcion.component.ts
+++ src/app/adopcion/adopcion.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './adopcion.component.html',
   styleUrls: ['./adopcion.component.scss']
 })
+
 export class AdopcionComponent implements OnInit {
   lectura:boolean;
 
@@ -18,5 +19,33 @@ export class AdopcionComponent implements OnInit {
   popUpLectura(): void{
     this.lectura = !this.lectura;
   }
+  mascotas:Array<Mascota> = [new Mascota("assets/images/pfp.png", "Hembra", "Lina", "Gato", 2, "Mestizo", false, "Toledo"),
+                        new Mascota("assets/images/perfil_gato.png", "Lina", "Hembra", "Gato", 2, "Mestizo", false, "Toledo"),
+                        new Mascota("assets/images/perfil_gato.png", "Lina", "Hembra", "Gato", 2, "Mestizo", false, "Toledo"),
+                        new Mascota("assets/images/perfil_gato.png", "Lina", "Hembra", "Gato", 2, "Mestizo", false, "Toledo"),];
 
+}
+
+
+//vamos a crear un objeto 'mascota' a ver que sale de aqui lol
+export class Mascota {
+  foto:any; 
+  nombre:string;
+  sexo:string;
+  tipo:string;
+  edad:number;
+  raza:string; 
+  boton:boolean;
+  ciudad:string;
+
+  constructor(foto:any, nombre:string, tipo:string, sexo:string, edad:number, raza:string, boton:boolean, ciudad:string){
+    this.foto = foto;
+    this.nombre = nombre;
+    this.sexo = sexo;
+    this.tipo = tipo;
+    this.edad = edad;
+    this.raza = raza;
+    this.boton = boton;
+    this.ciudad = ciudad
+  }
 }
