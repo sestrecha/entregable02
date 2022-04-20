aa41cd1691b3f6aefebc73260637006408ef0e69
maria15bm <100429055@alumnos.uc3m.es>
2022-04-20 23:11:53 +0200

    no puedo más

--- src/app/perfil/modificar/modificar.component.ts
+++ src/app/perfil/modificar/modificar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import * as $ from 'jquery';
 
 @Component({
@@ -10,26 +10,29 @@ import * as $ from 'jquery';
 export class ModificarComponent{
   salir:boolean = false;
   difuminar: boolean = false;
+
   datos = new FormGroup({
     pfp : new FormControl(''),
     descripcion : new FormControl(''),
     email : new FormControl('', [Validators.required,
-      Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+      Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:\
+        [a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
     ]),
     tlf : new FormControl('', [Validators.required,
     Validators.pattern("[0-9]{9}")]),
-    n_masc : new FormControl(''),
-    hogar : new FormControl(''),
-    hogar_other : new FormControl(''),
-    disc : new FormControl(''),
-    disc_other : new FormControl(''),
-    horas_libres : new FormControl(''),
-    tamanyo_casa : new FormControl(''),
-    estilo : new FormControl('')
+    n_masc : new FormControl(),
+    hogar : new FormControl(),
+    hogar_other : new FormControl(),
+    disc : new FormControl(),
+    disc_other : new FormControl(),
+    horas_libres : new FormControl(),
+    tamanyo_casa : new FormControl(),
+    estilo : new FormControl('sedentario', Validators.required)
     
   })
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
 
   get f()
   {
@@ -41,14 +44,10 @@ export class ModificarComponent{
     this.difuminar = this.salir;
   }
 
-  abrirPfp(event: any): void{
-    event.preventDefault(); 
-    $('#pfp').click();
-  }
-
   onSubmit(){
+    console.log(this.datos.value);
     if (this.datos.valid) {
-      console.log("Form Submitted!");
+      console.log(this.f.estilo);
     }
 
   }
