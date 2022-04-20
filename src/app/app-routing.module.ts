5471199d927e9288089574b823024b02789aa472
sestrecha <100429079@alumnos.uc3m.es>
2022-04-20 17:33:25 +0200

    pop up registrado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,6 @@ import { PortadaComponent } from './portada/portada.component';
 import { SeguimientoComponent } from './seguimiento/seguimiento.component';
 import { RescateComponent } from './rescate/rescate.component';
 import { FormulariorescateComponent } from './rescate/formulariorescate/formulariorescate.component';
-import { PopUpIniComponent } from './portada/pop-up-ini/pop-up-ini.component';
-import { PopUpRegComponent } from './portada/pop-up-reg/pop-up-reg.component';
 import { ModificarComponent } from './perfil/modificar/modificar.component';
 import { AdopcionComponent } from './adopcion/adopcion.component';
 import { ContactoComponent } from './contacto/contacto.component';
@@ -15,8 +13,6 @@ import { MasInfoComponent } from './adopcion/mas-info/mas-info.component';
 const routes: Routes = [
   { path:'perfil', component:PerfilComponent },
   { path:'portada', component:PortadaComponent },
-  { path:'pop-up-ini', component:PopUpIniComponent },
-  { path:'pop-up-reg',  component:PopUpRegComponent },
   { path:'perfil/modificar',  component:ModificarComponent },
   { path:'seguimiento',  component:SeguimientoComponent },
   { path:'rescate',  component:RescateComponent },
