92f6851de0686b9f23eec96578fb04a00dc0acca
sestrecha <100429079@alumnos.uc3m.es>
2022-04-20 17:51:22 +0200

    cositas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { SeguimientoComponent } from './seguimiento/seguimiento.component';
 import { RescateComponent } from './rescate/rescate.component';
 import { FormulariorescateComponent } from './rescate/formulariorescate/formulariorescate.component';
 import { ModificarComponent } from './perfil/modificar/modificar.component';
+import { CrearComponent } from './perfil/crear/crear.component';
 import { AdopcionComponent } from './adopcion/adopcion.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { MasInfoComponent } from './adopcion/mas-info/mas-info.component';
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path:'perfil', component:PerfilComponent },
   { path:'portada', component:PortadaComponent },
   { path:'perfil/modificar',  component:ModificarComponent },
+  { path:'perfil/crear',  component:CrearComponent },
   { path:'seguimiento',  component:SeguimientoComponent },
   { path:'rescate',  component:RescateComponent },
   { path:'formulariorescate',  component:FormulariorescateComponent },
