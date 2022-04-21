1928a3e8d93a088d5582eddfd258072115cac7c3
sestrecha <100429079@alumnos.uc3m.es>
2022-04-21 13:00:57 +0200

    menu desplegable :)

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  // menu desplegable
+  menu:boolean = false;
+  difuminar:boolean = false;
+  
+  displayMenu(): void {
+    this.menu = true;
+    this.difuminar = true;
+  }
+
+  hideMenu(): void {
+    this.menu = false;
+    this.difuminar = false;
+  }
 
   constructor() { }
 
--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent implements OnInit {
+  // menu desplegable
+  menu:boolean = false;
+  difuminar:boolean = false;
+  
+  displayMenu(): void {
+    this.menu = true;
+    this.difuminar = true;
+  }
+
+  hideMenu(): void {
+    this.menu = false;
+    this.difuminar = false;
+  }
+  
   ayuda: boolean;
 
   constructor() {
