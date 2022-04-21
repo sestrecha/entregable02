02fa85ca6f1993cf84e049a19235cefe80e06308
sestrecha <100429079@alumnos.uc3m.es>
2022-04-21 18:04:51 +0200

    creacion de perfil parte1

--- src/app/portada/pop-up-reg/pop-up-reg.component.ts
+++ src/app/portada/pop-up-reg/pop-up-reg.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from 'src/main';
 
 @Component({
   selector: 'app-pop-up-reg',
@@ -20,12 +22,37 @@ export class PopUpRegComponent implements OnInit {
     this.go_to_ini = true;
     this.go_to_ini_event.emit(this.go_to_ini);
   }
-  
-  
 
+  usuario:any;
+  formulario = new FormGroup({
+    usuario : new FormControl(''),
+    email : new FormControl('', [Validators.required,
+      Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:\
+        [a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+    ]),
+    pass : new FormControl('', [Validators.required,
+    Validators.pattern("[0-9]{4}")]),
+    pass_rep : new FormControl('', [Validators.required,
+      Validators.pattern("[0-9]{4}")]),
+    
+  })
+  
   constructor() { }
 
-  ngOnInit(): void {
+  get f()
+  {
+      return this.formulario.controls;
   }
+
+  onSubmit(){
+    if (this.formulario.valid) {
+      this.usuario = new Usuario(this.f.usuario.value, this.f.pass.value, "", "", this.f.email.value, "", "", "",
+      "", "", "", "", "")
+      
+      this.usuario.guardarUsuario()
+    }
+  }
+
+  ngOnInit(): void {}
 
 }
